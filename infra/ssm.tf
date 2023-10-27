bf640ad1060f45eeffbb19d3bf04bb5f4b2c00b1
jahir-biz <jahir.uddin@bizcloudexperts.com>
2023-10-27 16:59:25 +0530

    bucket name moved to ssm

--- infra/ssm.tf
+++ infra/ssm.tf
@@ -1,12 +1,12 @@
-# resource "aws_ssm_parameter" "shipment-header-arn" {
-#   name  = "/omni-wt-rt-updates/${var.env}/shipment-header/ddb.arn"
-#   type  = "SecureString"
-#   value = aws_dynamodb_table.omni-wt-rt-shipment-header.arn
+resource "aws_ssm_parameter" "omni_tac_event_bucket_name" {
+  name  = "/omni-tac-index/${var.env}/event-processor/bucket-name"
+  type  = "String"
+  value = var.omni_tac_event_bucket_name
 
-#   tags = {
-#     Application = "Real Time Updates"
-#     CreatedBy   = "BizCloudExperts"
-#     Environment = var.env
-#     STAGE       = var.env
-#   }
-# }
+  tags = {
+    Application = "Omni Tac Index"
+    CreatedBy   = "BizCloudExperts"
+    Environment = var.env
+    STAGE       = var.env
+  }
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -17,3 +17,8 @@ variable "aws_account_number" {
   type     = string
   nullable = false
 }
+
+variable "omni_tac_event_bucket_name" {
+  type     = string
+  nullable = false
+}
